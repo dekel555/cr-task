1d64bff4bbc874ff4806ffbe3df8f69fc63fb78c
dekel555 <dekelm5@gmail.com>
2021-08-01 09:18:45 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,6 @@ python3 ./DataToJson.py
 
 docker-compose up -d
 
-docker exec -i mongodb sh -c 'mongoimport -c <users> -d <cr-db> --drop' < ./users.json
-
 docker exec mongodb sh -c 'mongoimport --db cr-db --collection users --drop --file /users.json --jsonArray'
 
 docker exec mongodb sh -c 'mongoexport --db cr-db -c users --out exData.json --forceTableScan'
